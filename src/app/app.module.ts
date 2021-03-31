c7b98cca21e8298059086a16ce6a7b512df927d2
Benjamin <patonacevedo@gmail.com>
2021-03-31 15:16:14 -0400

    Added empty blockchain class boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { UploadComponent } from './component/upload/upload.component';
 
 import { NgxFileDropModule } from 'ngx-file-drop';
+import { BlockchainComponent } from './component/blockchain/blockchain.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UploadComponent
+    UploadComponent,
+    BlockchainComponent
   ],
   imports: [
     BrowserModule,
--- src/app/models/blockchain.ts
+++ src/app/models/blockchain.ts
@@ -1,14 +1,27 @@
 import {Block} from './block';
+import * as SHA256 from 'crypto-js/sha256';
+import * as bitcoinMessage from 'bitcoinjs-message';
 
-const SHA256  = require('crypto-js/sha256');
+// const SHA256  = require('crypto-js/sha256');
 
 export class Blockchain {
   private chain: Block[];
+  private height: number;
+
 
   constructor(block: Block) {
     this.chain = [];
+    this.height = -1;
 
     this.addBlock(this.createGenesisBlock());
+    this.initChain();
+  }
+
+  private async initChain(): Promise<void> {
+    if (this.height === -1) {
+      const block = this.createGenesisBlock();
+      await this.addBlock(block);
+    }
   }
 
   private createGenesisBlock(): Block {
@@ -31,17 +44,62 @@ export class Blockchain {
     return new Date().getTime().toString().slice(0, -3);
   }
 
-  public addBlock(newBlock: Block): void {
+  private addBlock(newBlock: Block): Promise<unknown> {
 
-    newBlock.height = this.getChainHeight();
-    newBlock.timeStamp = this.setTimeStamp(); /// UTC t.s
+    return new Promise((res, rej) => {
+      newBlock.height = this.getChainHeight();
+      newBlock.timeStamp = this.setTimeStamp(); /// UTC t.s
 
-    if (this.chain.length > 0) {
-      newBlock.prevBlockHash = this.getPrevBlockHash();
-    }
+      if (this.chain.length > 0) {
+        newBlock.prevBlockHash = this.getPrevBlockHash();
+      }
 
-    newBlock.hash = this.hashBlock(newBlock);
+      newBlock.hash = this.hashBlock(newBlock);
 
-    this.chain.push(newBlock);
+      this.chain.push(newBlock);
+    });
+  }
+
+  private requestMessageOwnershipVerification(address: string): Promise<void> {
+    return new Promise((resolve) => {
+
+    });
+  }
+
+  private submitStar(address: string, message: string, signature: string, star: any): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+
+    });
+  }
+
+  private getBlockByHash(hash): Promise<void> {
+    return new Promise((resolve, reject) => {
+
+    });
+  }
+
+  private getBlockByHeight(height: number): Promise<Block> {
+    return new Promise((resolve, reject) => {
+      const block = this.chain.filter(p => p.height === height)[0];
+      if (block){
+        resolve(block);
+      } else {
+        resolve(null);
+      }
+    });
+  }
+
+  private getStarsByWalletAddress(address: string): Promise<void> {
+    const stars = [];
+    return new Promise((resolve, reject) => {
+
+    });
+  }
+
+  private validateChain(): Promise<void> {
+    const errorLog = [];
+    return new Promise(async (resolve, reject) => {
+
+    });
   }
 }
