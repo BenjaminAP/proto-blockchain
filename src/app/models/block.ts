e2132562d10d784543b5e9bd4a92d0ebb03ce395
Benjamin <patonacevedo@gmail.com>
2021-03-31 14:46:10 -0400

    Block class modified

--- src/app/models/block.ts
+++ src/app/models/block.ts
@@ -1,16 +1,30 @@
+const SHA256 = require('crypto-js/sha256');
+const hex2ascii = require('hex2ascii');
 
-export class Block {
+class Block {
   hash: string;
   height: number;
-  body: any[];
+  body: string;
   timeStamp: string;
   prevBlockHash: string;
 
   constructor(data: any) {
-    this.hash = '';
+    this.hash = null;
     this.height = 0;
-    this.body = data;
+    this.body = new Buffer(JSON.stringify(data)).toString('hex');
     this.timeStamp = '';
     this.prevBlockHash = '0x';
   }
+
+  public validate(): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+
+    });
+  }
+
+  public getBlockData(): void {
+
+  }
 }
+
+module .exports.Block = Block;
