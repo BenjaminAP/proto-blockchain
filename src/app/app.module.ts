2aa8e471b4298beedff4ebf1a1fd2c296dce5d4c
Ben Acevedo <ben.acevedo@solarwinds.com>
2021-04-05 12:34:31 -0400

    added toolbar header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,27 @@ import { UploadComponent } from './component/upload/upload.component';
 import { NgxFileDropModule } from 'ngx-file-drop';
 import { BlockchainComponent } from './component/blockchain/blockchain.component';
 import {HttpClientModule} from "@angular/common/http";
+import { WalletComponent } from './component/wallet/wallet.component';
+import { HeaderToolbarComponent } from './component/header-toolbar/header-toolbar.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     UploadComponent,
-    BlockchainComponent
+    BlockchainComponent,
+    WalletComponent,
+    HeaderToolbarComponent
   ],
   imports: [
     BrowserModule,
     NgxFileDropModule,
     HttpClientModule,
+    MatIconModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
