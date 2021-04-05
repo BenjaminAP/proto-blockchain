ca70eb9b97a9ba5ec30b6b875b9f00f3e9244103
Ben Acevedo <ben.acevedo@solarwinds.com>
2021-04-05 13:08:10 -0400

    started mnemonic material dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { NgxFileDropModule } from 'ngx-file-drop';
 import { BlockchainComponent } from './component/blockchain/blockchain.component';
 import {HttpClientModule} from "@angular/common/http";
 import { WalletComponent } from './component/wallet/wallet.component';
-import { HeaderToolbarComponent } from './component/header-toolbar/header-toolbar.component';
+import {HeaderToolbarComponent, Mnemonic_Dialog} from './component/header-toolbar/header-toolbar.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from "@angular/material/dialog";
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     UploadComponent,
     BlockchainComponent,
     WalletComponent,
-    HeaderToolbarComponent
+    HeaderToolbarComponent,
+    Mnemonic_Dialog
   ],
   imports: [
     BrowserModule,
@@ -27,6 +29,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     MatIconModule,
     MatToolbarModule,
+    MatDialogModule,
     BrowserAnimationsModule,
   ],
   providers: [],
--- src/app/component/header-toolbar/header-toolbar.component.ts
+++ src/app/component/header-toolbar/header-toolbar.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-header-toolbar',
@@ -7,9 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  public openDialog(): void {
+    const dialogRef = this.dialog.open(Mnemonic_Dialog, {
+      width: '250px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+}
+
+@Component({
+  selector: 'mnemonic-dialog',
+  templateUrl: 'mnemonic_dialog.html',
+})
+export class Mnemonic_Dialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<Mnemonic_Dialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
