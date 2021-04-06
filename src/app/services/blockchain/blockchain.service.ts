5ac321adc328c346909e8c96d2c0de7c58203a0a
Ben Acevedo <ben.acevedo@solarwinds.com>
2021-04-06 18:44:21 -0400

    fixed post for submit star

--- src/app/services/blockchain/blockchain.service.ts
+++ src/app/services/blockchain/blockchain.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 
@@ -9,22 +9,32 @@ import {Observable} from 'rxjs';
 export class BlockchainService {
 
   readonly server_route: string;
+  // readonly  httpOptions = {
+  //   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  // };
+
+  public tempData = {
+    address: 'test',
+    message: 'test msg',
+    signature: 'test signature',
+    star: {
+      dec: 'test dec',
+      ra: 'test ra',
+      story: 'story test'}
+  }
 
   constructor(private http: HttpClient) {
     this.server_route = 'http://localhost:3000';
   }
 
   public requestMessageOwnershipVerification(pubAddress: string): Observable<string> {
-    
+
     return this.http.get(`${this.server_route}/signature/request/${pubAddress}`, {responseType: 'text'})
       .pipe(map((signatureOwnership: string) => signatureOwnership));
   }
-  
+
   public submitStar(): void {
     console.log('submitting star');
-    this.http.post(`${this.server_route}/submitStar`, {
-      params: {address: 'test', message: 'test msg', signature: 'test signature',
-        star: JSON.stringify({dec: 'test dec', ra: 'test ra', story: 'story test'})}
-    });
+    this.http.post<any>(`${this.server_route}/submitStar`, this.tempData).subscribe(temp => console.log(temp));
   }
 }
